d5c3ec6f131b62831b3a5701b768084075bfd066
edan <emanueledancelli@gmail.com>
2018-12-05 19:08:41 +0100

    refactor: removed useless comments

--- inc/dashboard.php
+++ inc/dashboard.php
@@ -3,16 +3,16 @@
 function remove_menus(){
     $current_user = wp_get_current_user();
    if( 'emanueledancelli@gmail.com' !==  $current_user->user_email){
-       remove_menu_page('edit.php'); // Posts
-       remove_menu_page('upload.php'); // Media
-       remove_menu_page('link-manager.php'); // Links
-       remove_menu_page('edit-comments.php'); // Comments
-       remove_menu_page('edit.php?post_type=page'); // Pages
-       remove_menu_page('plugins.php'); // Plugins
-       remove_menu_page('themes.php'); // Appearance
-       remove_menu_page('users.php'); // Users
-       remove_menu_page('tools.php'); // Tools
-       remove_menu_page('options-general.php'); // Settings
+       remove_menu_page('edit.php'); 
+       remove_menu_page('upload.php'); 
+       remove_menu_page('link-manager.php'); 
+       remove_menu_page('edit-comments.php');
+       remove_menu_page('edit.php?post_type=page'); 
+       remove_menu_page('plugins.php');
+       remove_menu_page('themes.php');
+       remove_menu_page('users.php'); 
+       remove_menu_page('tools.php'); 
+       remove_menu_page('options-general.php');
        remove_menu_page('profile.php');
    }
 }
