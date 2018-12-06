3166bbc9e5eb0d5d8af1f41cfcfad9e7512344ba
edan <emanueledancelli@gmail.com>
2018-12-06 09:27:24 +0100

    refactor: partials

--- inc/dashboard.php
+++ inc/dashboard.php
@@ -2,7 +2,7 @@
 
 function remove_menus(){
     $current_user = wp_get_current_user();
-   if( 'emanueledancelli@gmail.com' !==  $current_user->user_email){
+    if( 'emanueledancelli@gmail.com' !==  $current_user->user_email){
        remove_menu_page('edit.php'); 
        remove_menu_page('upload.php'); 
        remove_menu_page('link-manager.php'); 
@@ -18,40 +18,27 @@ function remove_menus(){
 }
 add_action( 'admin_menu', 'remove_menus' );
 
-function annointed_admin_bar_remove() {
-       global $wp_admin_bar;
-
-       $wp_admin_bar->remove_menu('wp-logo');
-}
-add_action('wp_before_admin_bar_render', 'annointed_admin_bar_remove', 0);
-
-function ya_do_it_admin_bar_remove() {
-   global $wp_admin_bar;
-   $wp_admin_bar->remove_menu('edit-profile');
-   $wp_admin_bar->remove_menu('user-info');
-}
-add_action('wp_before_admin_bar_render', 'ya_do_it_admin_bar_remove', 0);
-
-add_filter( 'admin_footer_text', '__return_empty_string', 11 ); 
-add_filter( 'update_footer', '__return_empty_string', 11 );
-
-
-function my_admin_bar_render() {
+function remove_admin_bar_content() {
     global $wp_admin_bar;
     $wp_admin_bar->remove_menu('comments');
-	$wp_admin_bar->remove_menu('new-content');
+    $wp_admin_bar->remove_menu('new-content');
+    $wp_admin_bar->remove_menu('edit-profile');
+    $wp_admin_bar->remove_menu('user-info');
+    $wp_admin_bar->remove_menu('wp-logo');
 }
-add_action( 'wp_before_admin_bar_render', 'my_admin_bar_render' );
+add_action( 'wp_before_admin_bar_render', 'remove_admin_bar_content' );
 
-add_filter( 'contextual_help', 'mytheme_remove_help_tabs', 999, 3 );
-function mytheme_remove_help_tabs($old_help, $screen_id, $screen){
+function remove_help_tabs($old_help, $screen_id, $screen){
     $screen->remove_help_tabs();
     return $old_help;
 }
-
-add_filter('screen_options_show_screen', '__return_false');
+add_filter( 'contextual_help', 'remove_help_tabs', 999, 3 );
 
 function hide_permalink() {
     return '';
 }
 add_filter( 'get_sample_permalink_html', 'hide_permalink' );
+
+add_filter( 'screen_options_show_screen', '__return_false');
+add_filter( 'admin_footer_text', '__return_empty_string', 11 ); 
+add_filter( 'update_footer', '__return_empty_string', 11 );
--- inc/login.php
+++ inc/login.php
@@ -1,34 +1,25 @@
 
 <?php
 
-//php-game is weak
+function custom_login() {
 
-function custom_logo() {
-	echo '<link rel="stylesheet" type="text/css" href="' . get_bloginfo('stylesheet_directory') . '/login.css" />';
-	}
-add_action('login_head', 'custom_logo');
-
-function remove_shake() {
 	remove_action('login_head', 'wp_shake_js', 12);
-	}
-add_action('login_head', 'remove_shake');
+	echo '<link rel="stylesheet" type="text/css" href="' . get_bloginfo('stylesheet_directory') . '/login.css" />';
+	
+}
+add_action('login_head', 'custom_login');
 
 function error_override() {
 	return 'Dati non corretti';
-	}
+}
 add_filter('login_errors', 'error_override');
 
-function check_remember_me() {
-	add_filter( 'login_footer', 'rememberme_checked' );
-}
-add_action( 'init', 'check_remember_me' );
-
-function my_login_logo_url() {
+function login_logo_url() {
 	return get_bloginfo( 'url' );
 }
-add_filter( 'login_headerurl', 'my_login_logo_url' );
+add_filter( 'login_headerurl', 'login_logo_url' );
 	
-function my_login_logo_url_title() {
+function login_logo_url_title() {
 	return 'Alture.org';
 }
-add_filter( 'login_headertitle', 'my_login_logo_url_title' );
+add_filter( 'login_headertitle', 'login_logo_url_title' );
